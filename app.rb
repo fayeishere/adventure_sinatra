3a89dacf803ebd2e76f752c512ba701d16b87588
Faye Purdum <fayepurdum@gmail.com>
2013-04-23 12:08:00 -0700

    init

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'sinatra'
+require 'json'
+
+# get the index page
+get "/" do
+  erb :index
+end
+
+# create a new something
+put '/something' do
+  render :form
+end
+
+# update a something
+post '/somethingelse' do
+end
+
+# delete a something
+delete '/somethingelse' do
+end
