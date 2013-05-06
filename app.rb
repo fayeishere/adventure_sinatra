8345d97d480664109d7f23c6a7e794749cc3dc4a
Faye Purdum <fayepurdum@gmail.com>
2013-05-06 01:31:21 -0700

    silly js and such

--- app.rb
+++ app.rb
@@ -5,15 +5,31 @@ require 'json'
 # get the index page
 get "/" do
   # send_file File.join('views', 'index.html')
-  erb :index, :layout => :layout
+  erb :index, :layout => :main_layout
 end
 
 # get a choice page
 get "/one" do
-    # send_file File.join('views', 'one.html')
-  erb :one, :layout => :layout
+  erb :one, :layout => :main_layout
 end
 
+get "/one_continues" do
+  erb :one_continues, :layout => :main_layout
+end
+
+get "/two" do
+  erb :index, :layout => :main_layout
+end
+
+get "/three" do
+  erb :index, :layout => :main_layout
+end
+
+post '/one' do
+  # puts "hidden: " + params[:hidden]
+  # puts "_method: " + params[:_method]
+  redirect "/one_continues"
+end
 # create a new something
 put '/something' do
   render :form
--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
     function initialize() {
     // alert("I am an alert box!");
     console.log('Hello');
+      $("#choice").append("Your decision is inspiring. Go to the corner of 'no one cares' and 'you will get there someday'");
+      $("#mermaid").append("You meet a mermaid who is not missing any fingers. What should you do?");
       if (true) {
-        $("#test").append("<p>This is an amazing choice!</p>");
+        $("#test").append("<p>You are quite the character. Nothing gets in your way. This is an amazing choice!</p>");
       }
       if (true) {
-      $("#again").append("<p>You are choosing amazingly!</p>");
+        $("#again").append("<p>You are choosing amazingly!</p>");
+      }
+      if (true) {
+        $(".no").append("<p>This choice is kind of stupid. Pick something else!</p>");
       }
     }
 });
